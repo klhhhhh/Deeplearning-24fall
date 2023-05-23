601e3d560a05657b7cfbe7b7ba3fe05c0042c40d
Xu-yyyyya <1198823730@qq.com>
2023-05-23 08:58:19 +0800

    add gcblock by stage for res18

--- xuyuang/ImproveforGC+SE/GC-SENet-18.py
+++ xuyuang/ImproveforGC+SE/GC-SENet-18.py
@@ -214,15 +214,15 @@ class ResNet(nn.Module):
         out = self.reslayer1_2(out)
         
         out = self.reslayer2_1(out)
-        out = self.gclayer2(out)
+        #out = self.gclayer2(out)
         out = self.reslayer2_2(out)
         
         out = self.reslayer3_1(out)
-        out = self.gclayer3(out)
+        #out = self.gclayer3(out)
         out = self.reslayer3_2(out)
         
         out = self.reslayer4_1(out)
-        out = self.gclayer4(out)
+        #out = self.gclayer4(out)
         out = self.reslayer4_2(out)
         
         out = self.pool(out)
