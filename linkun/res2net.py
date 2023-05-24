ce418509c95bff9d15fcaefb0a950572e7059e40
lk <lk@lkdeMacBook-Pro.local>
2023-05-25 01:43:32 +0800

    add bottleneck

--- linkun/res2net.py
+++ linkun/res2net.py
@@ -17,3 +17,59 @@ class SEModule(nn.Module):
         x = self.fc2(x)
         x = self.sigmoid(x)
         return input * x
+
+
+class Res2NetBottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, inplanes, planes, downsample=None, stride=1, scales=4, groups=1, se=False,  norm_layer=None):
+        super(Res2NetBottleneck, self).__init__()
+        if planes % scales != 0:
+            raise ValueError('Planes must be divisible by scales')
+        if norm_layer is None:
+            norm_layer = nn.BatchNorm2d
+        bottleneck_planes = groups * planes
+        self.conv1 = conv1x1(inplanes, bottleneck_planes, stride)
+        self.bn1 = norm_layer(bottleneck_planes)
+        self.conv2 = nn.ModuleList([conv3x3(bottleneck_planes // scales, bottleneck_planes // scales, groups=groups) for _ in range(scales-1)])
+        self.bn2 = nn.ModuleList([norm_layer(bottleneck_planes // scales) for _ in range(scales-1)])
+        self.conv3 = conv1x1(bottleneck_planes, planes * self.expansion)
+        self.bn3 = norm_layer(planes * self.expansion)
+        self.relu = nn.ReLU(inplace=True)
+        self.se = SEModule(planes * self.expansion) if se else None
+        self.downsample = downsample
+        self.stride = stride
+        self.scales = scales
+
+    def forward(self, x):
+        identity = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        xs = torch.chunk(out, self.scales, 1)
+        ys = []
+        for s in range(self.scales):
+            if s == 0:
+                ys.append(xs[s])
+            elif s == 1:
+                ys.append(self.relu(self.bn2[s-1](self.conv2[s-1](xs[s]))))
+            else:
+                ys.append(self.relu(self.bn2[s-1](self.conv2[s-1](xs[s] + ys[-1]))))
+        out = torch.cat(ys, 1)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        if self.se is not None:
+            out = self.se(out)
+
+        if self.downsample is not None:
+            identity = self.downsample(identity)
+
+        out += identity
+        out = self.relu(out)
+
+        return out
+
